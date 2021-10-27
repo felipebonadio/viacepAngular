cafa1146a22483faa5237f2978f0d717d3576758
felipebonadio <felipe.bonadio@etec.sp.gov.br>
2021-10-27 19:42:43 -0300

    2 files modified

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Cep, ceps } from '../ceps';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  cep : Cep | undefined;
 
-  constructor() { }
+  getCep(cep:string) {
+    const routeParams = this.route.snapshot.paramMap;
+    const numeroCepFromRoute = String(routeParams.get('numeroCep'));
+    this.cep = ceps.find(cep => cep.cep === numeroCepFromRoute);
+  }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  
 }
